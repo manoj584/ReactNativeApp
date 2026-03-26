5970dffc699ac6f738a59be52d61c255d29fd650
Bhaskar Manoj <manojbhaskar01234@gmail.com>
2026-03-26 12:04:54 +0530

    policy details update

--- android/app/src/main/java/com/bhaskarmanoj/reactnativeacademy/MainActivity.kt
+++ android/app/src/main/java/com/bhaskarmanoj/reactnativeacademy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bhaskarmanoj.LearnReactNative
+package com.bhaskarmanoj.reactnativeacademy
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/bhaskarmanoj/reactnativeacademy/MainApplication.kt
+++ android/app/src/main/java/com/bhaskarmanoj/reactnativeacademy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.bhaskarmanoj.LearnReactNative
+package com.bhaskarmanoj.reactnativeacademy
 
 import android.app.Application
 import android.content.res.Configuration
